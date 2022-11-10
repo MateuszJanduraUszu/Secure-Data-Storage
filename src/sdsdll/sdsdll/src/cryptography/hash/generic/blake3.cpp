7339c620ecf95e8ab0be83d4cf7270d9d2d3a8aa
MateuszJanduraUszu <Mati4321@interia.pl>
2022-11-10 15:01:15 +0100

    Rebuild BLAKE3 hash (#2)

--- src/sdsdll/sdsdll/src/cryptography/hash/generic/blake3.cpp
+++ src/sdsdll/sdsdll/src/cryptography/hash/generic/blake3.cpp
@@ -8,147 +8,71 @@
 #if _SDSDLL_PREPROCESSOR_GUARD
 
 _SDSDLL_BEGIN
-// FUNCTION TEMPLATE _Blake3_fn::operator()
-template <class _Simple_types>
-_NODISCARD constexpr bool _Blake3_fn<_Simple_types>::operator()(byte_type* const _Buf,
-    const size_type _Buf_size, const char_type* const _Data, const size_type _Data_size) const noexcept {
-    if (!_Buf || _Buf_size == 0) {
+// FUNCTION TEMPLATE blake3_traits::bytes_count
+template <class _Elem>
+_NODISCARD constexpr typename blake3_traits<_Elem>::size_type blake3_traits<_Elem>::bytes_count() noexcept {
+    return bits / CHAR_BIT; // always 32 bytes
+}
+
+// FUNCTION TEMPLATE blake3_traits::hash
+template <class _Elem>
+_NODISCARD constexpr bool blake3_traits<_Elem>::hash(byte_type* const _Buf,
+    const size_type _Buf_size, const char_type* const _Data, const size_type _Data_size) noexcept {
+    constexpr size_type _Optimal_buf_size = bytes_count();
+    if (!_Buf || _Buf_size < _Optimal_buf_size) {
         return false;
     }
 
     blake3_hasher _Hasher;
     ::blake3_hasher_init(_SDSDLL addressof(_Hasher));
     ::blake3_hasher_update(_SDSDLL addressof(_Hasher), _Data, _Data_size * sizeof(char_type));
-    ::blake3_hasher_finalize(_SDSDLL addressof(_Hasher), _Buf, _Buf_size);
+    ::blake3_hasher_finalize(_SDSDLL addressof(_Hasher), _Buf, _Optimal_buf_size);
     return true;
 }
 
-// FUNCTION TEMPLATE _Blake3_stream_fn::operator()
-template <class _Simple_types>
-_NODISCARD constexpr bool _Blake3_stream_fn<_Simple_types>::operator()(
-    blake3_hasher* const _Hasher, byte_type* const _Buf, const size_type _Buf_size,
-    const char_type* const _Data, const size_type _Data_size) const noexcept {
-    if (!_Hasher || !_Buf || _Buf_size == 0) {
+// FUNCTION TEMPLATE blake3_traits::hash_file
+template <class _Elem>
+_NODISCARD constexpr bool blake3_traits<_Elem>::hash_file(byte_type* const _Buf,
+    const size_type _Buf_size, file& _File, const file::off_type _Off) noexcept {
+    constexpr size_type _Optimal_buf_size = bytes_count();
+    if (!_Buf || _Buf_size < _Optimal_buf_size) {
         return false;
     }
 
-    ::blake3_hasher_update(_Hasher, _Data, _Data_size * sizeof(char_type));
-    return true;
-}
-
-// FUNCTION TEMPLATE _Blake3_default_engine copy constructor/destructor
-template <class _Elem>
-constexpr _Blake3_default_engine<_Elem>::_Blake3_default_engine(const _Elem* const _Data,
-    const size_type _Size) noexcept : _Mypair(_Ebco_default_init{}, size) {
-    if (!_Mypair._Val1._Empty()) { // buffer allocated, complete the task
-        _Release_on_failure(_Mypair._Val1,
-            _Mypair._Get_val2()(_Mypair._Val1._Get(), size, _Data, _Size));
+    if (!_File.is_open()) {
+        return false;
     }
-}
 
-template <class _Elem>
-_CONSTEXPR20 _Blake3_default_engine<_Elem>::~_Blake3_default_engine() noexcept {}
-
-// FUNCTION TEMPLATE _Blake3_default_engine::bytes
-template <class _Elem>
-_NODISCARD constexpr typename _Blake3_default_engine<_Elem>::byte_string
-    _Blake3_default_engine<_Elem>::bytes() const {
-    return !_Mypair._Val1._Empty() ? byte_string{_Mypair._Val1._Get(), size} : byte_string{};
-}
-
-// FUNCTION _Blake3_stream_default_engine constructor/destructor
-_Blake3_stream_default_engine::_Blake3_stream_default_engine() noexcept
-    : _Mypair(_Ebco_default_init{}, size) {
-    if (!_Mypair._Val1._Empty()) { // buffer allocated, try to allocate a hasher
-        allocator<blake3_hasher> _Al;
-        _Myhasher = _Al.allocate(1);
-        if (_Myhasher) { // hasher allocated, initialize it
-            ::blake3_hasher_init(_Myhasher);
+    if (_File.tell() != _Off) { // change file offset
+        if (!_File.seek(_Off)) {
+            return false;
         }
-    } else {
-        _Myhasher = nullptr;
-    }
-}
-
-_Blake3_stream_default_engine::~_Blake3_stream_default_engine() noexcept {
-    if (_Myhasher) {
-        allocator<blake3_hasher> _Al;
-        _Al.deallocate(_Myhasher, 1);
-        _Myhasher = nullptr;
-    }
-}
-
-// FUNCTION _Blake3_stream_default_engine::next_chunk
-_NODISCARD bool _Blake3_stream_default_engine::next_chunk(
-    const char_type* const _Chunk, const size_type _Count) noexcept {
-    if (!_Myhasher) { // invalid hasher
-        return false;
     }
 
-    return _Mypair._Get_val2()(_Myhasher, _Mypair._Val1._Get(), size, _Chunk, _Count);
-}
+    constexpr size_type _Chunk_size = 1024; // 1024-byte chunk
+    byte_type _Chunk[_Chunk_size];
+    size_type _Read = 0; // read bytes, must be initialized
+    blake3_hasher _Hasher;
+    ::blake3_hasher_init(_SDSDLL addressof(_Hasher));
+    for (;;) {
+        if (!_File.read(_Chunk, _Chunk_size, _Chunk_size, &_Read)) {
+            return false;
+        }
 
-// FUNCTION _Blake3_stream_default_engine::bytes
-_NODISCARD typename _Blake3_stream_default_engine::byte_string
-    _Blake3_stream_default_engine::bytes() {
-    if (!_Myhasher) { // invalid hasher
-        return byte_string{};
+        if (_Read == 0) { // no more data
+            break;
+        }
+
+        ::blake3_hasher_update(_SDSDLL addressof(_Hasher), _Chunk, _Read);
     }
 
-    ::blake3_hasher_finalize(_Myhasher, _Mypair._Val1._Get(), size);
-    return byte_string{_Mypair._Val1._Get(), size};
+    ::blake3_hasher_finalize(_SDSDLL addressof(_Hasher), _Buf, _Optimal_buf_size);
+    return true;
 }
 
-// FUNCTION TEMPLATE blake3
-template <class _Elem, class _Eng>
-_NODISCARD constexpr byte_string blake3(const _Elem* const _Data, const size_t _Count) {
-    return _Hash_data<_Elem, _Eng>(_Data, _Count);
-}
-
-template _SDSDLL_API _NODISCARD byte_string blake3(const char* const, const size_t);
-template _SDSDLL_API _NODISCARD byte_string blake3(const wchar_t* const, const size_t);
-
-template <class _Elem, class _Eng>
-_NODISCARD constexpr byte_string blake3(const _Elem* const _Data) {
-    using _Size_t = typename _Eng::size_type;
-    using _Traits = string_traits<_Elem, _Size_t>;
-    return _Hash_data<_Elem, _Eng>(_Data, _Traits::length(_Data));
-}
-
-template _SDSDLL_API _NODISCARD byte_string blake3(const char* const);
-template _SDSDLL_API _NODISCARD byte_string blake3(const wchar_t* const);
-
-template <class _Elem, class _Eng>
-_NODISCARD constexpr byte_string blake3(const basic_string_view<_Elem> _Data) {
-    return _Hash_data<_Elem, _Eng>(_Data.data(), _Data.size());
-}
-
-template _SDSDLL_API _NODISCARD byte_string blake3(const string_view);
-template _SDSDLL_API _NODISCARD byte_string blake3(const wstring_view);
-
-template <class _Elem, class _Eng>
-_NODISCARD constexpr byte_string blake3(const basic_string<_Elem>& _Data) {
-    return _Hash_data<_Elem, _Eng>(_Data.c_str(), _Data.size());
-}
-
-template _SDSDLL_API _NODISCARD byte_string blake3(const string&);
-template _SDSDLL_API _NODISCARD byte_string blake3(const wstring&);
-
-// FUNCTION TEMPLATE blake3_file
-template <class _Eng>
-_NODISCARD constexpr byte_string blake3_file(file& _File, const size_t _Off) {
-    return _Hash_file<_Eng>(_File, _Off);
-}
-
-template _SDSDLL_API _NODISCARD byte_string blake3_file(file&, const size_t);
-
-template <class _Eng>
-_NODISCARD constexpr byte_string blake3_file(const path& _Target, const size_t _Off) {
-    file _File(_Target);
-    return _Hash_file<_Eng>(_File, _Off);
-}
-
-template _SDSDLL_API _NODISCARD byte_string blake3_file(const path&, const size_t);
+template _SDSDLL_API struct blake3_traits<char>;
+template _SDSDLL_API struct blake3_traits<unsigned char>;
+template _SDSDLL_API struct blake3_traits<wchar_t>;
 _SDSDLL_END
 
 #endif // _SDSDLL_PREPROCESSOR_GUARD
--- src/sdsdll/sdsdll/src/cryptography/hash/generic/blake3.hpp
+++ src/sdsdll/sdsdll/src/cryptography/hash/generic/blake3.hpp
@@ -11,161 +11,45 @@
 #include <blake3.h>
 #include <climits>
 #include <core/api.hpp>
-#include <core/memory/allocator.hpp>
-#include <core/optimization/ebco.hpp>
-#include <core/optimization/sbo.hpp>
-#include <core/optimization/string_view.hpp>
 #include <core/traits/concepts.hpp>
 #include <core/traits/string_traits.hpp>
 #include <core/traits/type_traits.hpp>
-#include <cryptography/hash/hash_types.hpp>
 #include <cstddef>
 #include <filesystem/file.hpp>
-#include <filesystem/path.hpp>
 #include <string>
 
 // STD types
 using _STD basic_string;
-using _STD string;
-using _STD wstring;
 
 _SDSDLL_BEGIN
-// CLASS TEMPLATE _Blake3_fn
-template <class _Simple_types>
-class _Blake3_fn { // wrapper for the blake3_hasher functions
-public:
-    using char_type       = typename _Simple_types::char_type;
-    using byte_type       = typename _Simple_types::byte_type;
-    using char_string     = typename _Simple_types::char_string;
-    using byte_string     = typename _Simple_types::byte_string;
-    using size_type       = typename _Simple_types::size_type;
-    using difference_type = typename _Simple_types::difference_type;
-    using engine          = typename _Simple_types::engine;
-
-    constexpr _Blake3_fn() noexcept     = default;
-    _CONSTEXPR20 ~_Blake3_fn() noexcept = default;
-
-    _Blake3_fn(const _Blake3_fn&) = delete;
-    _Blake3_fn& operator=(const _Blake3_fn&) = delete;
-
-    // tries to invoke the blake3_hasher functions
-    _NODISCARD constexpr bool operator()(byte_type* const _Buf, const size_type _Buf_size,
-        const char_type* const _Data, const size_type _Data_size) const noexcept;
-};
-
-// CLASS TEMPLATE _Blake3_stream_fn
-template <class _Simple_types>
-class _Blake3_stream_fn { // wrapper for the blake3_hasher_update() function
-public:
-    using char_type       = typename _Simple_types::char_type;
-    using byte_type       = typename _Simple_types::byte_type;
-    using char_string     = typename _Simple_types::char_string;
-    using byte_string     = typename _Simple_types::byte_string;
-    using size_type       = typename _Simple_types::size_type;
-    using difference_type = typename _Simple_types::difference_type;
-    using engine          = typename _Simple_types::engine;
-
-    constexpr _Blake3_stream_fn() noexcept     = default;
-    _CONSTEXPR20 ~_Blake3_stream_fn() noexcept = default;
-
-    _Blake3_stream_fn(const _Blake3_stream_fn&) = delete;
-    _Blake3_stream_fn& operator=(const _Blake3_stream_fn&) = delete;
-
-    // tries to invoke the blake3_hasher_update() function
-    _NODISCARD constexpr bool operator()(
-        blake3_hasher* const _Hasher, byte_type* const _Buf, const size_type _Buf_size,
-        const char_type* const _Data, const size_type _Data_size) const noexcept;
-};
-
-// CLASS TEMPLATE _Blake3_default_engine
+// STRUCT TEMPLATE blake3_traits
 template <class _Elem>
-class _Blake3_default_engine { // default engine for the BLAKE3 algorithm
+struct _SDSDLL_API blake3_traits { // traits for the BLAKE3 hash
 private:
-    static_assert(is_any_of_v<_Elem, char, wchar_t>, "Requires UTF-8/UTF-16 element type.");
-
-    using _Simple_types = _Simple_hash_types<_Elem, _Blake3_default_engine<_Elem>>;
-    using _Fn           = _Blake3_fn<_Simple_types>;
+    static_assert(is_any_of_v<_Elem, char, unsigned char, wchar_t>,
+        "Requires a UTF-8/Unicode element type.");
 
 public:
-    using char_type       = typename _Simple_types::char_type;
-    using byte_type       = typename _Simple_types::byte_type;
-    using char_string     = typename _Simple_types::char_string;
-    using byte_string     = typename _Simple_types::byte_string;
-    using size_type       = typename _Simple_types::size_type;
-    using difference_type = typename _Simple_types::difference_type;
-    using engine          = typename _Simple_types::engine;
+    using char_type       = _Elem;
+    using byte_type	      = unsigned char;
+    using char_string     = basic_string<_Elem>;
+    using byte_string     = basic_string<unsigned char>;
+    using size_type       = size_t;
+    using difference_type = ptrdiff_t;
 
-    // always a 256-bit (32-byte) ASCII string
-    static constexpr size_type size = 256 / CHAR_BIT;
+    static constexpr size_type bits = 256;
 
-    constexpr _Blake3_default_engine(const _Elem* const _Data, const size_type _Size) noexcept;
-    _CONSTEXPR20 ~_Blake3_default_engine() noexcept;
+    // returns hashed text length
+    _NODISCARD static constexpr size_type bytes_count() noexcept;
 
-    _Blake3_default_engine() = delete;
-    _Blake3_default_engine(const _Blake3_default_engine&) = delete;
-    _Blake3_default_engine& operator=(const _Blake3_default_engine&) = delete;
+    // hashes a UTF-8/Unicode text
+    _NODISCARD static constexpr bool hash(byte_type* const _Buf, const size_type _Buf_size,
+        const char_type* const _Data, const size_type _Data_size) noexcept;
 
-    // returns hashed data or an empty string
-    _NODISCARD constexpr byte_string bytes() const;
-
-private:
-    _Ebco_pair<_Sbo_buffer<byte_type>, _Fn> _Mypair;
+    // hashes a file
+    _NODISCARD static constexpr bool hash_file(byte_type* const _Buf,
+        const size_type _Buf_size, file& _File, const file::off_type _Off) noexcept;
 };
-
-// CLASS _Blake3_stream_default_engine
-class _Blake3_stream_default_engine { // default engine for the stream BLAKE3 algorithm
-private:
-    using _Simple_types = _Simple_hash_types<char, _Blake3_stream_default_engine>;
-    using _Fn           = _Blake3_stream_fn<_Simple_types>;
-
-public:
-    using char_type       = typename _Simple_types::char_type;
-    using byte_type       = typename _Simple_types::byte_type;
-    using char_string     = typename _Simple_types::char_string;
-    using byte_string     = typename _Simple_types::byte_string;
-    using size_type       = typename _Simple_types::size_type;
-    using difference_type = typename _Simple_types::difference_type;
-    using engine          = typename _Simple_types::engine;
-
-    // always a 256-bit (32-byte) ASCII string
-    static constexpr size_type size = 256 / CHAR_BIT;
-
-    _Blake3_stream_default_engine() noexcept;
-    ~_Blake3_stream_default_engine() noexcept;
-
-    _Blake3_stream_default_engine(const _Blake3_stream_default_engine&) = delete;
-    _Blake3_stream_default_engine& operator=(const _Blake3_stream_default_engine&) = delete;
-
-    // hashes the next chunk
-    _NODISCARD bool next_chunk(const char_type* const _Chunk, const size_type _Count) noexcept;
-
-    // returns hashed data or an empty string
-    _NODISCARD byte_string bytes();
-
-private:
-    _Ebco_pair<_Sbo_buffer<byte_type>, _Fn> _Mypair;
-    blake3_hasher* _Myhasher;
-};
-
-// FUNCTION TEMPLATE blake3
-template <class _Elem, class _Eng = _Blake3_default_engine<_Elem>>
-_SDSDLL_API _NODISCARD constexpr byte_string blake3(const _Elem* const _Data, const size_t _Count);
-
-template <class _Elem, class _Eng = _Blake3_default_engine<_Elem>>
-_SDSDLL_API _NODISCARD constexpr byte_string blake3(const _Elem* const _Data);
-
-template <class _Elem, class _Eng = _Blake3_default_engine<_Elem>>
-_SDSDLL_API _NODISCARD constexpr byte_string blake3(const basic_string_view<_Elem> _Data);
-
-template <class _Elem, class _Eng = _Blake3_default_engine<_Elem>>
-_SDSDLL_API _NODISCARD constexpr byte_string blake3(const basic_string<_Elem>& _Data);
-
-// FUNCTION TEMPLATE blake3_file
-template <class _Eng = _Blake3_stream_default_engine>
-_SDSDLL_API _NODISCARD constexpr byte_string blake3_file(file& _File, const size_t _Off = 0);
-
-template <class _Eng = _Blake3_stream_default_engine>
-_SDSDLL_API _NODISCARD constexpr byte_string blake3_file(const path& _Target, const size_t _Off = 0);
 _SDSDLL_END
 
 #endif // _SDSDLL_PREPROCESSOR_GUARD
