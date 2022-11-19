d4be3edc82dcce0b66c819b9044eb2b375d84cc4
MateuszJanduraUszu <Mati4321@interia.pl>
2022-11-19 16:15:06 +0100

    Rebuild Scrypt password hash (#2)

--- src/sdsdll/sdsdll/src/cryptography/hash/password/scrypt.cpp
+++ src/sdsdll/sdsdll/src/cryptography/hash/password/scrypt.cpp
@@ -8,13 +8,20 @@
 #if _SDSDLL_PREPROCESSOR_GUARD
 
 _SDSDLL_BEGIN
-// FUNCTION TEMPLATE _Scrypt_fn::operator()
-template <class _Simple_types>
-_NODISCARD constexpr bool _Scrypt_fn<_Simple_types>::operator()(byte_type* const _Buf,
-    const size_type _Buf_size, const char* const _Data, const size_type _Data_size,
-    const byte_type* const _Salt, const size_type _Salt_size, const size_type _Cost,
-    const size_type _Block_size, const size_type _Parallelism) const noexcept {
-    if (!_Buf || _Buf_size == 0) {
+// FUNCTION TEMPLATE scrypt_traits::bytes_count
+template <class _Elem>
+_NODISCARD constexpr typename scrypt_traits<_Elem>::size_type
+    scrypt_traits<_Elem>::bytes_count() noexcept {
+    return bits / CHAR_BIT; // always 64 bytes
+}
+
+// FUNCTION TEMPLATE scrypt_traits::hash
+template <class _Elem>
+_NODISCARD constexpr bool scrypt_traits<_Elem>::hash(byte_type* const _Buf, const size_type _Buf_size,
+    const char_type* const _Data, const size_type _Data_size, const salt& _Salt,
+    const size_type _Cost, const size_type _Block_size, const size_type _Parallelism) noexcept {
+    constexpr size_type _Optimal_buf_size = bytes_count();
+    if (!_Buf || _Buf_size < _Optimal_buf_size) {
         return false;
     }
 
@@ -30,91 +37,29 @@ _NODISCARD constexpr bool _Scrypt_fn<_Simple_types>::operator()(byte_type* const
         return false;
     }
 
-    try {
-        ::Botan::scrypt( // throws if one of the arguments is invalid
-            _Buf, _Buf_size, _Data, _Data_size, _Salt, _Salt_size, _Cost, _Block_size, _Parallelism);
-        return true;
-    } catch (...) { // handler not used
-        return false;
-    }
-}
-
-// FUNCTION TEMPLATE _Scrypt_default_engine copy constructor/destructor
-template <class _Elem>
-constexpr _Scrypt_default_engine<_Elem>::_Scrypt_default_engine(
-    const _Elem* const _Data, const size_type _Size, const byte_type* const _Salt,
-    const size_type _Cost, const size_type _Block_size, const size_type _Parallelism) noexcept
-    : _Mypair(_Ebco_default_init{}, size) {
-    if (!_Mypair._Val1._Empty()) { // buffer allocated, complete the task
-        if _CONSTEXPR_IF (is_same_v<_Elem, char>) {
-            _Release_on_failure(_Mypair._Val1, _Mypair._Get_val2()(
-                _Mypair._Val1._Get(), size, _Data, _Size, _Salt,
-                    salt_size, _Cost, _Block_size, _Parallelism));
-        } else {
-            const utf8_string& _Narrow = utf8_string::from_utf16(_Data, _Size);
-            _Release_on_failure(_Mypair._Val1, _Mypair._Get_val2()(
-                _Mypair._Val1._Get(), size, _Narrow.c_str(), _Narrow.size(),
-                    _Salt, salt_size, _Cost, _Block_size, _Parallelism));
+    if _CONSTEXPR_IF (sizeof(_Elem) == 1) { // hash a UTF-8 password
+        try { // throws if one of the arguments is invalid
+            ::Botan::scrypt(_Buf, _Optimal_buf_size, reinterpret_cast<const char*>(_Data),
+                _Data_size, _Salt.get(), _Salt.size, _Cost, _Block_size, _Parallelism);
+        } catch (...) {
+            return false;
+        }
+    } else { // hash a Unicode password
+        const utf8_string& _Narrow = utf8_string::from_utf16(_Data, _Data_size);
+        try { // throws if one of the arguments is invalid
+            ::Botan::scrypt(_Buf, _Optimal_buf_size, _Narrow.c_str(), _Narrow.size(),
+                _Salt.get(), _Salt.size, _Cost, _Block_size, _Parallelism);
+        } catch (...) {
+            return false;
         }
     }
+
+    return true;
 }
 
-template <class _Elem>
-_CONSTEXPR20 _Scrypt_default_engine<_Elem>::~_Scrypt_default_engine() noexcept {}
-
-// FUNCTION TEMPLATE _Scrypt_default_engine::bytes
-template <class _Elem>
-_NODISCARD constexpr typename _Scrypt_default_engine<_Elem>::byte_string
-    _Scrypt_default_engine<_Elem>::bytes() const {
-    return !_Mypair._Val1._Empty() ? byte_string{_Mypair._Val1._Get(), size} : byte_string{};
-}
-
-// auxiliary macro for sdsdll::scrypt() function export/import
-#define _EXPORT_OR_IMPORT_SCRYPT(_Elem, _Str, ...)                        \
-    template _SDSDLL_API _NODISCARD byte_string scrypt(_Str, __VA_ARGS__, \
-        const salt<_Scrypt_default_engine<_Elem>>&, const size_t, const size_t, const size_t)
-
-// FUNCTION TEMPLATE scrypt
-template <class _Elem, class _Eng>
-_NODISCARD constexpr byte_string scrypt(
-    const _Elem* const _Data, const size_t _Size, const salt<_Eng>& _Salt,
-    const size_t _Cost, const size_t _Block_size, const size_t _Parallelism) {
-    return _Hash_password<_Elem, _Eng>(_Data, _Size, _Salt, _Cost, _Block_size, _Parallelism);
-}
-
-_EXPORT_OR_IMPORT_SCRYPT(char, const char* const, const size_t);
-_EXPORT_OR_IMPORT_SCRYPT(wchar_t, const wchar_t* const, const size_t);
-
-template <class _Elem, class _Eng>
-_NODISCARD constexpr byte_string scrypt(const _Elem* const _Data, const salt<_Eng>& _Salt,
-    const size_t _Cost, const size_t _Block_size, const size_t _Parallelism) {
-    using _Size_t = typename _Eng::size_type;
-    using _Traits = string_traits<_Elem, _Size_t>;
-    return _Hash_password<_Elem, _Eng>(
-        _Data, _Traits::length(_Data), _Salt, _Cost, _Block_size, _Parallelism);
-}
-
-_EXPORT_OR_IMPORT_SCRYPT(char, const char* const);
-_EXPORT_OR_IMPORT_SCRYPT(wchar_t, const wchar_t* const);
-
-template <class _Elem, class _Eng>
-_NODISCARD constexpr byte_string scrypt(const basic_string_view<_Elem> _Data, const salt<_Eng>& _Salt,
-    const size_t _Cost, const size_t _Block_size, const size_t _Parallelism) {
-    return _Hash_password<_Elem, _Eng>(_Data.data(), _Data.size(), _Salt, _Cost, _Block_size, _Parallelism);
-}
-
-_EXPORT_OR_IMPORT_SCRYPT(char, const string_view);
-_EXPORT_OR_IMPORT_SCRYPT(wchar_t, const wstring_view);
-
-template <class _Elem, class _Eng>
-_NODISCARD constexpr byte_string scrypt(const basic_string<_Elem>& _Data, const salt<_Eng>& _Salt,
-    const size_t _Cost, const size_t _Block_size, const size_t _Parallelism) {
-    return _Hash_password<_Elem, _Eng>(_Data.c_str(), _Data.size(), _Salt, _Cost, _Block_size, _Parallelism);
-}
-
-_EXPORT_OR_IMPORT_SCRYPT(char, const string&);
-_EXPORT_OR_IMPORT_SCRYPT(wchar_t, const wstring&);
-#undef _EXPORT_OR_IMPORT_SCRYPT
+template _SDSDLL_API struct scrypt_traits<char>;
+template _SDSDLL_API struct scrypt_traits<unsigned char>;
+template _SDSDLL_API struct scrypt_traits<wchar_t>;
 _SDSDLL_END
 
 #endif // _SDSDLL_PREPROCESSOR_GUARD
--- src/sdsdll/sdsdll/src/cryptography/hash/password/scrypt.hpp
+++ src/sdsdll/sdsdll/src/cryptography/hash/password/scrypt.hpp
@@ -9,110 +9,46 @@
 #include <core/defs.hpp>
 #if _SDSDLL_PREPROCESSOR_GUARD
 #include <botan/scrypt.h>
+#include <climits>
 #include <core/api.hpp>
 #include <core/memory/allocator.hpp>
-#include <core/optimization/ebco.hpp>
-#include <core/optimization/sbo.hpp>
-#include <core/optimization/string_view.hpp>
 #include <core/traits/concepts.hpp>
 #include <core/traits/string_traits.hpp>
-#include <cryptography/hash/hash_types.hpp>
+#include <cryptography/random/salt.hpp>
 #include <cstddef>
 #include <encoding/utf8.hpp>
 #include <string>
 
 // STD types
 using _STD basic_string;
-using _STD string;
-using _STD wstring;
 
 _SDSDLL_BEGIN
-// CLASS TEMPLATE _Scrypt_fn
-template <class _Simple_types>
-class _Scrypt_fn { // wrapper for the Botan::scrypt() function
-public:
-    using char_type       = typename _Simple_types::char_type;
-    using byte_type       = typename _Simple_types::byte_type;
-    using char_string     = typename _Simple_types::char_string;
-    using byte_string     = typename _Simple_types::byte_string;
-    using size_type       = typename _Simple_types::size_type;
-    using difference_type = typename _Simple_types::difference_type;
-    using engine          = typename _Simple_types::engine;
-
-    constexpr _Scrypt_fn() noexcept     = default;
-    _CONSTEXPR20 ~_Scrypt_fn() noexcept = default;
-
-    _Scrypt_fn(const _Scrypt_fn&) = delete;
-    _Scrypt_fn& operator=(const _Scrypt_fn&) = delete;
-
-    // tries to invoke the Botan::scrypt() function
-    _NODISCARD constexpr bool operator()(byte_type* const _Buf, const size_type _Buf_size,
-        const char* const _Data, const size_type _Data_size, const byte_type* const _Salt,
-        const size_type _Salt_size, const size_type _Cost, const size_type _Block_size,
-        const size_type _Parallelism) const noexcept;
-};
-
-// CLASS TEMPLATE _Scrypt_default_engine
+// STRUCT TEMPLATE scrypt_traits
 template <class _Elem>
-class _Scrypt_default_engine { // default engine for the Scrypt algorithm
+struct _SDSDLL_API scrypt_traits { // traits for the Scrypt password hash
 private:
-    static_assert(is_any_of_v<_Elem, char, wchar_t>, "Requires UTF-8/UTF-16 element type.");
-
-    using _Simple_types = _Simple_hash_types<_Elem, _Scrypt_default_engine>;
-    using _Fn           = _Scrypt_fn<_Simple_types>;
+    static_assert(is_any_of_v<_Elem, char, unsigned char, wchar_t>,
+        "Requires a byte/UTF-8/Unicode element type.");
 
 public:
-    using char_type       = typename _Simple_types::char_type;
-    using byte_type       = typename _Simple_types::byte_type;
-    using char_string     = typename _Simple_types::char_string;
-    using byte_string     = typename _Simple_types::byte_string;
-    using size_type       = typename _Simple_types::size_type;
-    using difference_type = typename _Simple_types::difference_type;
-    using engine          = typename _Simple_types::engine;
+    using char_type       = _Elem;
+    using byte_type	      = unsigned char;
+    using char_string     = basic_string<_Elem>;
+    using byte_string     = basic_string<unsigned char>;
+    using size_type       = size_t;
+    using difference_type = ptrdiff_t;
+    using salt            = salt<16>;
 
-    static constexpr size_type size      = 64;
-    static constexpr size_type salt_size = 16;
+    static constexpr size_type bits = 512;
 
-    constexpr _Scrypt_default_engine(const _Elem* const _Data, const size_type _Size,
-        const byte_type* const _Salt, const size_type _Cost, const size_type _Block_size,
-        const size_type _Parallelism) noexcept;
-    _CONSTEXPR20 ~_Scrypt_default_engine() noexcept;
+    // returns hashed password length
+    _NODISCARD static constexpr size_type bytes_count() noexcept;
 
-    _Scrypt_default_engine() = delete;
-    _Scrypt_default_engine(const _Scrypt_default_engine&) = delete;
-    _Scrypt_default_engine& operator=(const _Scrypt_default_engine&) = delete;
-
-    // returns hashed data or an empty string
-    _NODISCARD constexpr byte_string bytes() const;
-
-private:
-    _Ebco_pair<_Sbo_buffer<byte_type>, _Fn> _Mypair;
+    // hashes a UTF-8/Unicode password
+    _NODISCARD static constexpr bool hash(byte_type* const _Buf, const size_type _Buf_size,
+        const char_type* const _Data, const size_type _Data_size, const salt& _Salt,
+        const size_type _Cost, const size_type _Block_size, const size_type _Parallelism) noexcept;
 };
-
-// CLASS TEMPLATE salt
-template <class>
-class salt;
-
-// FUNCTION TEMPLATE scrypt
-template <class _Elem, class _Eng = _Scrypt_default_engine<_Elem>>
-_SDSDLL_API _NODISCARD constexpr byte_string scrypt(
-    const _Elem* const _Data, const size_t _Size, const salt<_Eng>& _Salt,
-    const size_t _Cost = 16384, const size_t _Block_size = 16, const size_t _Parallelism = 1);
-
-template <class _Elem, class _Eng = _Scrypt_default_engine<_Elem>>
-_SDSDLL_API _NODISCARD constexpr byte_string scrypt(
-    const _Elem* const _Data, const salt<_Eng>& _Salt, const size_t _Cost = 16384,
-    const size_t _Block_size = 16, const size_t _Parallelism = 1);
-
-template <class _Elem, class _Eng = _Scrypt_default_engine<_Elem>>
-_SDSDLL_API _NODISCARD constexpr byte_string scrypt(
-    const basic_string_view<_Elem> _Data, const salt<_Eng>& _Salt, const size_t _Cost = 16384,
-    const size_t _Block_size = 16, const size_t _Parallelism = 1);
-
-template <class _Elem, class _Eng = _Scrypt_default_engine<_Elem>>
-_SDSDLL_API _NODISCARD constexpr byte_string scrypt(
-    const basic_string<_Elem>& _Data, const salt<_Eng>& _Salt, const size_t _Cost = 16384,
-    const size_t _Block_size = 16, const size_t _Parallelism = 1);
 _SDSDLL_END
 
 #endif // _SDSDLL_PREPROCESSOR_GUARD
