3e8c8244eb43800077162e15a3fe9a3c0306bd53
MateuszJanduraUszu <Mati4321@interia.pl>
2022-11-26 22:57:51 +0100

    Rebuild AES-256-GCM cipher (#2)

--- src/sdsdll/sdsdll/src/cryptography/cipher/symmetric/aes256_gcm.cpp
+++ src/sdsdll/sdsdll/src/cryptography/cipher/symmetric/aes256_gcm.cpp
@@ -8,255 +8,129 @@
 #if _SDSDLL_PREPROCESSOR_GUARD
 
 _SDSDLL_BEGIN
-// FUNCTION _Get_aes256_gcm_tag
-_NODISCARD bool _Get_aes256_gcm_tag(_Cipher_context_proxy& _Proxy, uint8_t* const _Buf) noexcept {
-    if (!_Proxy._Ctx || !_Buf) { // invalid context or buffer
-        return false;
-    }
-
-    // Note: It is important to exclude the tag from the context, because
-    //       with it the EVP_DecryptFinal_ex() will return 0 (failure).
-    return ::EVP_CIPHER_CTX_ctrl(_Proxy._Ctx, EVP_CTRL_GCM_GET_TAG, 16, _Buf) != 0; // 16-byte tag
+// FUNCTION TEMPLATE aes256_gcm_traits::bytes_count
+template <class _Elem>
+_NODISCARD constexpr typename aes256_gcm_traits<_Elem>::size_type
+    aes256_gcm_traits<_Elem>::bytes_count(const size_type _Count) noexcept {
+    return _Count + _Tag_size; // always plain text length + 16-byte tag
 }
 
-// FUNCTION _Set_aes256_gcm_tag
-_NODISCARD bool _Set_aes256_gcm_tag(_Cipher_context_proxy& _Proxy, uint8_t* const _Tag) noexcept {
-    if (!_Proxy._Ctx || !_Tag) { // invalid context or tag
-        return false;
-    }
-
-    return ::EVP_CIPHER_CTX_ctrl(_Proxy._Ctx, EVP_CTRL_GCM_SET_TAG, 16, _Tag) != 0; // 16-byte tag
+// FUNCTION TEMPLATE aes256_gcm_traits::chars_count
+template <class _Elem>
+_NODISCARD constexpr typename aes256_gcm_traits<_Elem>::size_type
+    aes256_gcm_traits<_Elem>::chars_count(const size_type _Count) noexcept {
+    return _Count - _Tag_size; // always cipher text length - 16-byte tag
 }
 
-// FUNCTION TEMPLATE _Aes256_gcm_fn::operator()
-template <class _Simple_types>
-_NODISCARD constexpr bool _Aes256_gcm_fn<_Simple_types>::operator()(
-    byte_type* const _Buf, size_type _Buf_size, const byte_type* const _Data, const size_type _Data_size,
-    const key& _Key, const iv& _Iv, const cipher_task _Task) const noexcept {
-    if (!_Buf || _Buf_size == 0) { // invalid buffer
+// FUNCTION TEMPLATE aes256_gcm_traits::encrypt
+template <class _Elem>
+_NODISCARD constexpr bool aes256_gcm_traits<_Elem>::encrypt(
+    byte_type* const _Buf, const size_type _Buf_size, const char_type* const _Data,
+    const size_type _Data_size, const key& _Key, const iv& _Iv) noexcept {
+    if (!_Buf) {
         return false;
     }
 
-    _Cipher_context_proxy _Proxy;
-    if (!_Proxy._Ctx) { // failed to allocated the context
+    byte_string _Raw_data;
+    if _CONSTEXPR_IF (sizeof(_Elem) == 1) { // encrypt a UTF-8 string
+        _Raw_data.assign(reinterpret_cast<const byte_type*>(_Data), _Data_size);
+    } else { // encrypt a Unicode string
+        const utf8_string& _Narrow = utf8_string::from_utf16(_Data, _Data_size);
+        _Raw_data.assign(reinterpret_cast<const byte_type*>(_Narrow.c_str()), _Narrow.size());
+    }
+
+    const size_type _Optimal_buf_size = bytes_count(_Raw_data.size());
+    if (_Buf_size < _Optimal_buf_size) {
         return false;
     }
 
-    if (_Task == cipher_task::encryption) { // try to encrypt the data
-        if (::EVP_EncryptInit_ex(_Proxy._Ctx, ::EVP_aes_256_gcm(), nullptr, _Key.get(), _Iv.get()) == 0) {
-            return false;
-        }
+    _Aead_cipher_context_proxy _Proxy;
+    if (!_Proxy._Ctx) {
+        return false;
+    }
 
-        constexpr size_type _Tag_size = 16;
-        _Buf_size                    -= _Tag_size; // skip storage for the tag
-        if (::EVP_EncryptUpdate(_Proxy._Ctx, _Buf, reinterpret_cast<int*>(&_Buf_size),
-            _Data, static_cast<int>(_Data_size)) == 0) {
-            return false;
-        }
+    if (::EVP_EncryptInit_ex(_Proxy._Ctx, ::EVP_aes_256_gcm(), nullptr, _Key.get(), _Iv.get()) == 0) {
+        return false;
+    }
 
-        int _Bytes; // encrypted bytes (unused)
-        if (::EVP_EncryptFinal_ex(_Proxy._Ctx, _Buf + _Buf_size, &_Bytes) == 0) {
-            return false;
-        }
+    int _Bytes = 0; // encrypted bytes (unused)
+    if (::EVP_EncryptUpdate(
+        _Proxy._Ctx, _Buf, &_Bytes, _Raw_data.c_str(), static_cast<int>(_Raw_data.size())) == 0) {
+        return false;
+    }
 
-        // Note: Not extracted AES-256 GCM tag will cause an error in the EVP_DecryptFinal_ex() function.
-        //       The tag should be extracted and concated with the encrypted data.
-        byte_type _Tag[_Tag_size];
-        if (!_Get_aes256_gcm_tag(_Proxy, _Tag)) {
-            return false;
-        }
+    if (::EVP_EncryptFinal_ex(_Proxy._Ctx, _Buf + _Bytes, &_Bytes) == 0) {
+        return false;
+    }
 
-        // Note: Real buffer size is _Buf_size + _Tag_size so it is safe to
-        //       move tag data to the _Buf + _Buf_size location.
-        memory_traits::move(_Buf + _Buf_size, _Tag, _Tag_size * sizeof(byte_type));
-    } else { // try to decrypt the data
-        if (::EVP_DecryptInit_ex(_Proxy._Ctx, ::EVP_aes_256_gcm(), nullptr, _Key.get(), _Iv.get()) == 0) {
-            return false;
-        }
+    return _Get_aead_tag(_Proxy, _Buf + (_Optimal_buf_size - _Tag_size));
+}
 
-        constexpr size_type _Tag_size = 16;
-        if (::EVP_DecryptUpdate(_Proxy._Ctx, _Buf, reinterpret_cast<int*>(&_Buf_size),
-            _Data, static_cast<int>(_Data_size - _Tag_size)) == 0) {
-            return false;
-        }
+// FUNCTION TEMPLATE aes256_gcm_traits::decrypt
+template <class _Elem>
+_NODISCARD constexpr bool aes256_gcm_traits<_Elem>::decrypt(char_type* const _Buf,
+    const size_type _Buf_size, const byte_type* const _Data, const size_type _Data_size,
+    const key& _Key, const iv& _Iv, size_type* const _Count) noexcept {
+    if (!_Buf) {
+        return false;
+    }
 
-        // Note: The tag must be set in the context and the data must not contains it,
-        //       otherwise the EVP_DecryptFinal_ex() will return 0 (failure).
-        byte_type _Tag[_Tag_size];
-        memory_traits::move(_Tag, _Data + (_Data_size - _Tag_size), _Tag_size * sizeof(byte_type));
-        if (!_Set_aes256_gcm_tag(_Proxy, _Tag)) {
-            return false;
-        }
-
-        int _Bytes; // decrypted bytes (unused)
-        if (::EVP_DecryptFinal_ex(_Proxy._Ctx, _Buf + _Buf_size, &_Bytes) == 0) {
+    const size_type _Optimal_buf_size = chars_count(_Data_size);
+    if _CONSTEXPR_IF (sizeof(_Elem) == 1) {
+        // Note: A UTF-8 string length is always equal to _Optimal_buf_size.
+        if (_Buf_size < _Optimal_buf_size) {
             return false;
         }
     }
-    
+
+    _Aead_cipher_context_proxy _Proxy;
+    if (!_Proxy._Ctx) {
+        return false;
+    }
+
+    if (::EVP_DecryptInit_ex(_Proxy._Ctx, ::EVP_aes_256_gcm(), nullptr, _Key.get(), _Iv.get()) == 0) {
+        return false;
+    }
+
+    _Sbo_buffer<byte_type> _Temp_buf(_Optimal_buf_size);
+    if (_Temp_buf._Empty()) { // failed to allocate a buffer
+        return false;
+    }
+
+    int _Bytes = 0; // decrypted bytes (unused)
+    if (::EVP_DecryptUpdate(_Proxy._Ctx,
+        _Temp_buf._Get(), &_Bytes, _Data, static_cast<int>(_Data_size - _Tag_size)) == 0) {
+        return false;
+    }
+
+    if (!_Set_aead_tag(_Proxy, _Data + (_Data_size - _Tag_size))) {
+        return false;
+    }
+
+    if (::EVP_DecryptFinal_ex(_Proxy._Ctx, _Temp_buf._Get() + _Bytes, &_Bytes) == 0) {
+        return false;
+    }
+
+    if _CONSTEXPR_IF (sizeof(_Elem) == 1) { // decrypt a UTF-8 string
+        memory_traits::copy(_Buf, _Temp_buf._Get(), _Optimal_buf_size);
+        if (_Count) {
+            *_Count = _Optimal_buf_size;
+        }
+    } else { // decrypt a Unicode string
+        const utf16_string& _Wide = utf16_string::from_utf8(
+            reinterpret_cast<const char*>(_Temp_buf._Get()), _Temp_buf._Size());
+        memory_traits::copy(_Buf, _Wide.c_str(), _Wide.size() * sizeof(_Elem));
+        if (_Count) {
+            *_Count = _Wide.size();
+        }
+    }
+
     return true;
 }
 
-// FUNCTION TEMPLATE _Aes256_gcm_default_engine copy constructor/destructor
-template <class _Elem>
-constexpr _Aes256_gcm_default_engine<_Elem>::_Aes256_gcm_default_engine(
-    const _Elem* const _Data, const size_type _Size, const key& _Key, const iv& _Iv,
-    const cipher_task _Task) noexcept : _Mypair(_Ebco_default_init{}) {
-    if _CONSTEXPR_IF (is_same_v<_Elem, char>) {
-        const size_type _Buf_size = _Calculate_buffer_size(_Size, _Task);
-        if (_Mypair._Val1._Resize(_Buf_size)) { // buffer allocated, complete the task
-            _Release_on_failure(_Mypair._Val1, _Mypair._Get_val2()(_Mypair._Val1._Get(), _Buf_size,
-                reinterpret_cast<const byte_type*>(_Data), _Size, _Key, _Iv, _Task));
-        }
-    } else if _CONSTEXPR_IF (is_same_v<_Elem, uint8_t>) {
-        const size_type _Buf_size = _Calculate_buffer_size(_Size, _Task);
-        if (_Mypair._Val1._Resize(_Buf_size)) { // buffer allocated, complete the task
-            _Release_on_failure(_Mypair._Val1, _Mypair._Get_val2()(
-                _Mypair._Val1._Get(), _Buf_size, _Data, _Size, _Key, _Iv, _Task));
-        }
-    } else {
-        const utf8_string& _Narrow = utf8_string::from_utf16(_Data, _Size);
-        const size_type _Buf_size  = _Calculate_buffer_size(_Narrow.size(), _Task);
-        if (_Mypair._Val1._Resize(_Buf_size)) { // buffer allocated, complete the task
-            _Release_on_failure(_Mypair._Val1, _Mypair._Get_val2()(_Mypair._Val1._Get(), _Buf_size,
-                reinterpret_cast<const byte_type*>(_Narrow.c_str()), _Narrow.size(), _Key, _Iv, _Task));
-        }
-    }
-}
-
-template <class _Elem>
-_CONSTEXPR20 _Aes256_gcm_default_engine<_Elem>::~_Aes256_gcm_default_engine() noexcept {}
-
-// FUNCTION TEMPLATE _Aes256_gcm_default_engine::_Calculate_buffer_size
-template <class _Elem>
-_NODISCARD constexpr typename _Aes256_gcm_default_engine<_Elem>::size_type
-    _Aes256_gcm_default_engine<_Elem>::_Calculate_buffer_size(
-    const size_type _Data_size, const cipher_task _Task) const noexcept {
-    // encryption adds 16-byte tag and decryption removes it
-    return _Task == cipher_task::encryption ? _Data_size + 16 : _Data_size - 16;
-}
-
-// FUNCTION TEMPLATE _Aes256_gcm_default_engine::bytes
-template <class _Elem>
-_NODISCARD constexpr typename _Aes256_gcm_default_engine<_Elem>::byte_string
-    _Aes256_gcm_default_engine<_Elem>::bytes() const {
-    return !_Mypair._Val1._Empty() ?
-        byte_string{_Mypair._Val1._Get(), _Mypair._Val1._Size()} : byte_string{};
-}
-
-// FUNCTION TEMPLATE decrypt_aes256_gcm
-template <class _Elem, class _Eng>
-_NODISCARD constexpr basic_string<_Elem> decrypt_aes256_gcm(const uint8_t* const _Data,
-    const size_t _Count, const aes_key<32>& _Key, const iv<12>& _Iv) {
-    static_assert(is_any_of_v<_Elem, char, wchar_t>, "Requires UTF-8/UTF-16 element type.");
-    static_assert(is_same_v<uint8_t, typename _Eng::char_type>, "Incompatible type.");
-    static_assert(is_same_v<size_t, typename _Eng::size_type>, "Incompatible type.");
-    if (_Count == 0) { // empty string, do nothing
-        return basic_string<_Elem>{};
-    }
-
-    const byte_string& _Bytes = _Eng(_Data, _Count, _Key, _Iv, cipher_task::decryption).bytes();
-    if _CONSTEXPR_IF (is_same_v<_Elem, char>) {
-        return string{reinterpret_cast<const char*>(_Bytes.c_str()), _Bytes.size()};
-    } else {
-        const utf16_string& _Wide = utf16_string::from_utf8(
-            reinterpret_cast<const char*>(_Bytes.c_str()), _Bytes.size());
-        return _Wide.str();
-    }
-}
-
-template _SDSDLL_API _NODISCARD string decrypt_aes256_gcm(
-    const uint8_t* const, const size_t, const aes_key<32>&, const iv<12>&);
-template _SDSDLL_API _NODISCARD wstring decrypt_aes256_gcm(
-    const uint8_t* const, const size_t, const aes_key<32>&, const iv<12>&);
-
-template <class _Elem, class _Eng>
-_NODISCARD constexpr basic_string<_Elem> decrypt_aes256_gcm(
-    const uint8_t* const _Data, const aes_key<32>& _Key, const iv<12>& _Iv) {
-    using _Size_t = typename _Eng::size_type;
-    using _Traits = string_traits<uint8_t, _Size_t>;
-    return _SDSDLL decrypt_aes256_gcm<_Elem, _Eng>(_Data, _Traits::length(_Data), _Key, _Iv);
-}
-
-template _SDSDLL_API _NODISCARD string decrypt_aes256_gcm(
-    const uint8_t* const, const aes_key<32>&, const iv<12>&);
-template _SDSDLL_API _NODISCARD wstring decrypt_aes256_gcm(
-    const uint8_t* const, const aes_key<32>&, const iv<12>&);
-
-template <class _Elem, class _Eng>
-_NODISCARD constexpr basic_string<_Elem> decrypt_aes256_gcm(
-    const byte_string_view _Data, const aes_key<32>& _Key, const iv<12>& _Iv) {
-    return _SDSDLL decrypt_aes256_gcm<_Elem, _Eng>(_Data.data(), _Data.size(), _Key, _Iv);
-}
-
-template _SDSDLL_API _NODISCARD string decrypt_aes256_gcm(
-    const byte_string_view, const aes_key<32>&, const iv<12>&);
-template _SDSDLL_API _NODISCARD wstring decrypt_aes256_gcm(
-    const byte_string_view, const aes_key<32>&, const iv<12>&);
-
-template <class _Elem, class _Eng>
-_NODISCARD constexpr basic_string<_Elem> decrypt_aes256_gcm(
-    const byte_string& _Data, const aes_key<32>& _Key, const iv<12>& _Iv) {
-    return _SDSDLL decrypt_aes256_gcm<_Elem, _Eng>(_Data.c_str(), _Data.size(), _Key, _Iv);
-}
-
-template _SDSDLL_API _NODISCARD string decrypt_aes256_gcm(
-    const byte_string&, const aes_key<32>&, const iv<12>&);
-template _SDSDLL_API _NODISCARD wstring decrypt_aes256_gcm(
-    const byte_string&, const aes_key<32>&, const iv<12>&);
-
-// FUNCTION TEMPLATE encrypt_aes256_gcm
-template <class _Elem, class _Eng>
-_NODISCARD constexpr byte_string encrypt_aes256_gcm(
-    const _Elem* const _Data, const size_t _Count, const aes_key<32>& _Key, const iv<12>& _Iv) {
-    static_assert(is_same_v<_Elem, typename _Eng::char_type>, "Incompatible type.");
-    static_assert(is_same_v<size_t, typename _Eng::size_type>, "Incompatible type.");
-    if (_Count == 0) { // empty string, do nothing
-        return byte_string{};
-    }
-    
-    return _Eng(_Data, _Count, _Key, _Iv, cipher_task::encryption).bytes();
-}
-
-template _SDSDLL_API _NODISCARD byte_string encrypt_aes256_gcm(
-    const char* const, const size_t, const aes_key<32>&, const iv<12>&);
-template _SDSDLL_API _NODISCARD byte_string encrypt_aes256_gcm(
-    const wchar_t* const, const size_t, const aes_key<32>&, const iv<12>&);
-
-template <class _Elem, class _Eng>
-_NODISCARD constexpr byte_string encrypt_aes256_gcm(
-    const _Elem* const _Data, const aes_key<32>& _Key, const iv<12>& _Iv) {
-    using _Size_t = typename _Eng::size_type;
-    using _Traits = string_traits<_Elem, _Size_t>;
-    return _SDSDLL encrypt_aes256_gcm<_Elem, _Eng>(_Data, _Traits::length(_Data), _Key, _Iv);
-}
-
-template _SDSDLL_API _NODISCARD byte_string encrypt_aes256_gcm(
-    const char* const, const aes_key<32>&, const iv<12>&);
-template _SDSDLL_API _NODISCARD byte_string encrypt_aes256_gcm(
-    const wchar_t* const, const aes_key<32>&, const iv<12>&);
-
-template <class _Elem, class _Eng>
-_NODISCARD constexpr byte_string encrypt_aes256_gcm(
-    const basic_string_view<_Elem> _Data, const aes_key<32>& _Key, const iv<12>& _Iv) {
-    return _SDSDLL encrypt_aes256_gcm<_Elem, _Eng>(_Data.data(), _Data.size(), _Key, _Iv);
-}
-
-template _SDSDLL_API _NODISCARD byte_string encrypt_aes256_gcm(
-    const string_view, const aes_key<32>&, const iv<12>&);
-template _SDSDLL_API _NODISCARD byte_string encrypt_aes256_gcm(
-    const wstring_view, const aes_key<32>&, const iv<12>&);
-
-template <class _Elem, class _Eng>
-_NODISCARD constexpr byte_string encrypt_aes256_gcm(
-    const basic_string<_Elem>& _Data, const aes_key<32>& _Key, const iv<12>& _Iv) {
-    return _SDSDLL encrypt_aes256_gcm<_Elem, _Eng>(_Data.c_str(), _Data.size(), _Key, _Iv);
-}
-
-template _SDSDLL_API _NODISCARD byte_string encrypt_aes256_gcm(
-    const string&, const aes_key<32>&, const iv<12>&);
-template _SDSDLL_API _NODISCARD byte_string encrypt_aes256_gcm(
-    const wstring&, const aes_key<32>&, const iv<12>&);
+template struct _SDSDLL_API aes256_gcm_traits<char>;
+template struct _SDSDLL_API aes256_gcm_traits<unsigned char>;
+template struct _SDSDLL_API aes256_gcm_traits<wchar_t>;
 _SDSDLL_END
 
 #endif // _SDSDLL_PREPROCESSOR_GUARD
--- src/sdsdll/sdsdll/src/cryptography/cipher/symmetric/aes256_gcm.hpp
+++ src/sdsdll/sdsdll/src/cryptography/cipher/symmetric/aes256_gcm.hpp
@@ -9,130 +9,61 @@
 #include <core/defs.hpp>
 #if _SDSDLL_PREPROCESSOR_GUARD
 #include <core/api.hpp>
-#include <core/optimization/ebco.hpp>
+#include <core/container/bytes.hpp>
 #include <core/optimization/sbo.hpp>
-#include <core/optimization/string_view.hpp>
 #include <core/traits/concepts.hpp>
 #include <core/traits/memory_traits.hpp>
 #include <core/traits/string_traits.hpp>
-#include <cryptography/cipher/cipher_types.hpp>
-#include <cryptography/cipher/symmetric/aes_key.hpp>
+#include <cryptography/cipher/symmetric/aead.hpp>
 #include <cryptography/cipher/symmetric/iv.hpp>
+#include <cryptography/cipher/symmetric/symmetric_key.hpp>
 #include <cstddef>
 #include <encoding/utf8.hpp>
+#include <encoding/utf16.hpp>
 #include <openssl/evp.h>
+#include <openssl/types.h>
 #include <string>
 
 // STD types
 using _STD basic_string;
-using _STD string;
-using _STD wstring;
 
 _SDSDLL_BEGIN
-// FUNCTION _Get_aes256_gcm_tag
-extern _NODISCARD bool _Get_aes256_gcm_tag(_Cipher_context_proxy& _Proxy, uint8_t* const _Buf) noexcept;
-
-// FUNCTION _Set_aes256_gcm_tag
-extern _NODISCARD bool _Set_aes256_gcm_tag(_Cipher_context_proxy& _Proxy, uint8_t* const _Tag) noexcept;
-
-// CLASS TEMPLATE _Aes256_gcm_fn
-template <class _Simple_types>
-class _Aes256_gcm_fn { // wrapper for the EVP AES-256 GCM functions
-public:
-    using char_type       = typename _Simple_types::char_type;
-    using byte_type       = typename _Simple_types::byte_type;
-    using char_string     = typename _Simple_types::char_string;
-    using byte_string     = typename _Simple_types::byte_string;
-    using size_type       = typename _Simple_types::size_type;
-    using difference_type = typename _Simple_types::difference_type;
-    using engine          = typename _Simple_types::engine;
-    using key             = typename _Simple_types::key;
-    using iv              = typename _Simple_types::iv;
-
-    constexpr _Aes256_gcm_fn() noexcept     = default;
-    _CONSTEXPR20 ~_Aes256_gcm_fn() noexcept = default;
-
-    _Aes256_gcm_fn(const _Aes256_gcm_fn&) = delete;
-    _Aes256_gcm_fn& operator=(const _Aes256_gcm_fn&) = delete;
-
-    // tries to encrypt/decrypt the data
-    _NODISCARD constexpr bool operator()(byte_type* const _Buf, size_type _Buf_size,
-        const byte_type* const _Data, const size_type _Data_size, const key& _Key,
-        const iv& _Iv, const cipher_task _Task) const noexcept;
-};
-
-// CLASS TEMPLATE _Aes256_gcm_default_engine
+// STRUCT TEMPLATE aes256_gcm_traits
 template <class _Elem>
-class _Aes256_gcm_default_engine { // default engine for the AES-256 GCM algorithm
+struct _SDSDLL_API aes256_gcm_traits { // traits for the AES-256-GCM cipher
 private:
-    static_assert(is_any_of_v<_Elem, char, uint8_t, wchar_t>, "Requires UTF-8/UTF-16 element type.");
-
-    using _Simple_types = _Simple_cipher_types<_Elem, _Aes256_gcm_default_engine<_Elem>, aes_key<32>, iv<12>>;
-    using _Fn           = _Aes256_gcm_fn<_Simple_types>;
+    static_assert(is_any_of_v<_Elem, char, unsigned char, wchar_t>,
+        "Requires a byte/UTF-8/Unicode element type.");
 
 public:
-    using char_type       = typename _Simple_types::char_type;
-    using byte_type       = typename _Simple_types::byte_type;
-    using char_string     = typename _Simple_types::char_string;
-    using byte_string     = typename _Simple_types::byte_string;
-    using size_type       = typename _Simple_types::size_type;
-    using difference_type = typename _Simple_types::difference_type;
-    using engine          = typename _Simple_types::engine;
-    using key             = typename _Simple_types::key;
-    using iv              = typename _Simple_types::iv;
+    using char_type       = _Elem;
+    using byte_type	      = unsigned char;
+    using char_string     = basic_string<_Elem>;
+    using byte_string     = basic_string<unsigned char>;
+    using size_type       = size_t;
+    using difference_type = ptrdiff_t;
+    using key             = symmetric_key<32>;
+    using iv              = iv<12>;
 
-    constexpr _Aes256_gcm_default_engine(const _Elem* const _Data, const size_type _Size,
-        const key& _Key, const iv& _Iv, const cipher_task _Task) noexcept;
-    _CONSTEXPR20 ~_Aes256_gcm_default_engine() noexcept;
+    // returns encrypted text length
+    _NODISCARD static constexpr size_type bytes_count(const size_type _Count) noexcept;
 
-    _Aes256_gcm_default_engine() = delete;
-    _Aes256_gcm_default_engine(const _Aes256_gcm_default_engine&) = delete;
-    _Aes256_gcm_default_engine& operator=(const _Aes256_gcm_default_engine&) = delete;
+    // returns decrypted text length
+    _NODISCARD static constexpr size_type chars_count(const size_type _Count) noexcept;
 
-    // returns encrypted/decrypted data or an empty string
-    _NODISCARD constexpr byte_string bytes() const;
+    // encrypts a UTF-8/Unicode text
+    _NODISCARD static constexpr bool encrypt(
+        byte_type* const _Buf, const size_type _Buf_size, const char_type* const _Data,
+        const size_type _Data_size, const key& _Key, const iv& _Iv) noexcept;
+
+    // decrypts a UTF-8/Unicode text
+    _NODISCARD static constexpr bool decrypt(char_type* const _Buf,
+        const size_type _Buf_size, const byte_type* const _Data, const size_type _Data_size,
+        const key& _Key, const iv& _Iv, size_type* const _Count = nullptr) noexcept;
 
 private:
-    // calculates the buffer size for the AES-256 GCM encryption/decryption
-    _NODISCARD constexpr size_type _Calculate_buffer_size(
-        const size_type _Data_size, const cipher_task _Task) const noexcept;
-
-    _Ebco_pair<_Sbo_buffer<byte_type>, _Fn> _Mypair;
+    static constexpr size_type _Tag_size = 16; // 16-byte authentication tag
 };
-
-// FUNCTION TEMPLATE decrypt_aes256_gcm
-template <class _Elem, class _Eng = _Aes256_gcm_default_engine<uint8_t>>
-_SDSDLL_API _NODISCARD constexpr basic_string<_Elem> decrypt_aes256_gcm(
-    const uint8_t* const _Data, const size_t _Count, const aes_key<32>& _Key, const iv<12>& _Iv);
-
-template <class _Elem, class _Eng = _Aes256_gcm_default_engine<uint8_t>>
-_SDSDLL_API _NODISCARD constexpr basic_string<_Elem> decrypt_aes256_gcm(
-    const uint8_t* const _Data, const aes_key<32>& _Key, const iv<12>& _Iv);
-
-template <class _Elem, class _Eng = _Aes256_gcm_default_engine<uint8_t>>
-_SDSDLL_API _NODISCARD constexpr basic_string<_Elem> decrypt_aes256_gcm(
-    const byte_string_view _Data, const aes_key<32>& _Key, const iv<12>& _Iv);
-
-template <class _Elem, class _Eng = _Aes256_gcm_default_engine<uint8_t>>
-_SDSDLL_API _NODISCARD constexpr basic_string<_Elem> decrypt_aes256_gcm(
-    const byte_string& _Data, const aes_key<32>& _Key, const iv<12>& _Iv);
-
-// FUNCTION TEMPLATE encrypt_aes256_gcm
-template <class _Elem, class _Eng = _Aes256_gcm_default_engine<_Elem>>
-_SDSDLL_API _NODISCARD constexpr byte_string encrypt_aes256_gcm(
-    const _Elem* const _Data, const size_t _Count, const aes_key<32>& _Key, const iv<12>& _Iv);
-
-template <class _Elem, class _Eng = _Aes256_gcm_default_engine<_Elem>>
-_SDSDLL_API _NODISCARD constexpr byte_string encrypt_aes256_gcm(
-    const _Elem* const _Data, const aes_key<32>& _Key, const iv<12>& _Iv);
-
-template <class _Elem, class _Eng = _Aes256_gcm_default_engine<_Elem>>
-_SDSDLL_API _NODISCARD constexpr byte_string encrypt_aes256_gcm(
-    const basic_string_view<_Elem> _Data, const aes_key<32>& _Key, const iv<12>& _Iv);
-
-template <class _Elem, class _Eng = _Aes256_gcm_default_engine<_Elem>>
-_SDSDLL_API _NODISCARD constexpr byte_string encrypt_aes256_gcm(
-    const basic_string<_Elem>& _Data, const aes_key<32>& _Key, const iv<12>& _Iv);
 _SDSDLL_END
 
 #endif // _SDSDLL_PREPROCESSOR_GUARD
